899a9b5ad8accdafb4d20ed0b7245ce190936079
Brian Holestine <brian.holestine@intel.com>
2018-08-29 08:24:24 -0700

    Renaming

--- BananaEnvironment/dqn_agent.py
+++ BananaEnvironment/dqn_agent.py
@@ -83,12 +83,12 @@ class Agent():
             experiences (Tuple[torch.Variable]): tuple of (s, a, r, s', done) tuples 
             gamma (float): discount factor
         """
-        states, actions, rewards, next_states, dones = experiences
+        states, actions, rewards, next_states, done = experiences
 
         # Get max predicted Q values (for next states) from target model
         Q_targets_next = self.qnetwork_target(next_states).detach().max(1)[0].unsqueeze(1)
         # Compute Q targets for current states 
-        Q_targets = rewards + (gamma * Q_targets_next * (1 - dones))
+        Q_targets = rewards + (gamma * Q_targets_next * (1 - done))
 
         # Get expected Q values from local model
         Q_expected = self.qnetwork_local(states).gather(1, actions)
